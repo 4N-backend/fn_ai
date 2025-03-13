5ecfb7061d0926bd349f6b20fb045180f803c206
joon <tjrwns3428@gmail.com>
2025-03-13 14:59:04 +0900

    project init

---
+++ com.fn.ai.order/src/main/java/com/fn/ai/order/OrderApplication.java
@@ -0,0 +1,13 @@
+package com.fn.ai.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderApplication.class, args);
+	}
+
+}
