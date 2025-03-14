2836936f089d1c15cd86306584798d9d980c8885
letsgilit <wjdrlf5781@naver.com>
2025-03-15 02:29:32 +0900

    feat: auth 서버 인증인가 구현
    -Spring Security를 활용한 jwt검증 filter 및 login filter 구현
    -jwtUtil Class 구현

---
+++ com.fn.ai.auth/src/main/java/com/fn/ai/auth/security/UserDetailsServiceImpl.java
@@ -0,0 +1,28 @@
+package com.fn.ai.auth.security;
+
+import com.fn.ai.auth.application.client.dto.UserResponseDto;
+import com.fn.ai.auth.application.client.UserClient;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserClient userClient;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        UserResponseDto userResponseDto = userClient.getUserByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException(username));
+
+        return new UserDetailsImpl(UserResponseDto.of(
+                userResponseDto.id(),
+                userResponseDto.username(),
+                userResponseDto.password(),
+                userResponseDto.role()));
+    }
+}
---
+++ com.fn.ai.auth/src/main/java/com/fn/ai/auth/security/UserRoleEnum.java
@@ -0,0 +1,21 @@
+package com.fn.ai.auth.security;
+
+public enum UserRoleEnum {
+    MASTER(Authority.MASTER),
+    HUB_MANAGER(Authority.HUB_MANAGER),
+    COMPANY_MANAGER(Authority.COMPANY_MANAGER),
+    DELIVERY_MANAGER(Authority.DELIVERY_MANAGER);
+
+    private final String authority;
+
+    UserRoleEnum(String authority) { this.authority = authority; }
+
+    public String getAuthority(){ return this.authority; }
+
+    public static class Authority{
+        public static final String MASTER = "ROLE_MASTER";
+        public static final String HUB_MANAGER = "ROLE_HUB_MANAGER";
+        public static final String COMPANY_MANAGER = "ROLE_COMPANY_MANAGER";
+        public static final String DELIVERY_MANAGER= "ROLE_DELIVERY_MANAGER";
+    }
+}
