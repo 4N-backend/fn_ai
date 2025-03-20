802979db5b24d85a91c3532062e2483b5bf25ac5
cchoijjinyoung <cgy7127@gmail.com>
2025-03-20 18:44:11 +0900

    refactor: 배송 생성 수정

--- com.fn.ai.delivery/src/main/java/com/fn/ai/delivery/application/client/dto/HubRouteResponseDto.java
+++ com.fn.ai.delivery/src/main/java/com/fn/ai/delivery/application/client/dto/HubRouteResponseDto.java
@@ -1,19 +1,28 @@
 package com.fn.ai.delivery.application.client.dto;
 
+import com.fn.ai.delivery.model.DeliveryRoute;
 import java.util.List;
 import java.util.UUID;
 
 public record HubRouteResponseDto(
-    List<RouteStopInfo> routeStopInfos
+    List<RouteInfo> routeInfos
 ) {
 
-  public record RouteStopInfo(
+  public record RouteInfo(
       UUID departureHubId,
       UUID arrivalHubId,
       Double distance,
-      Long travelTime,
-      Long sequence
+      Long travelTime
   ) {
 
+    public DeliveryRoute toEntity(int sequence) {
+      return DeliveryRoute.of(
+          this.departureHubId(),
+          this.arrivalHubId(),
+          this.distance(),
+          this.travelTime(),
+          sequence
+      );
+    }
   }
 }
