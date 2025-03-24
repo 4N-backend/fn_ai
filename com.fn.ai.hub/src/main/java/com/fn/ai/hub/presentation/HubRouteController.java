ac00c559076bd288c8bd751aa43138706b524872
joon <tjrwns3428@gmail.com>
2025-03-24 16:56:22 +0900

    refactor(88) : hub-refactor
    hub service 리팩토링
    - 허브 루트 생ì생성 리팩토링
    - 권한 설정

--- com.fn.ai.hub/src/main/java/com/fn/ai/hub/presentation/HubRouteController.java
+++ com.fn.ai.hub/src/main/java/com/fn/ai/hub/presentation/HubRouteController.java
@@ -1,6 +1,7 @@
 package com.fn.ai.hub.presentation;
 
 import com.fn.ai.common.application.CommonResponse;
+import com.fn.ai.common.config.aop.RequireAuthorization;
 import com.fn.ai.common.exception.code.CommonResponseCode;
 import com.fn.ai.hub.application.HubRouteService;
 import com.fn.ai.hub.application.dto.request.HubRouteCreateRequestDto;
@@ -32,6 +33,7 @@ public class HubRouteController {
 
     @Operation(summary = "허브 루트 생성")
     @PostMapping("/")
+    @RequireAuthorization({"MASTER"})
     public ResponseEntity<CommonResponse<HubRouteResponseDto>> createHubRoute(@RequestBody
     HubRouteCreateRequestDto requestDto) {
 
@@ -42,6 +44,7 @@ public class HubRouteController {
 
     @Operation(summary = "허브 루트 변경")
     @PutMapping("/{route_id}")
+    @RequireAuthorization({"MASTER"})
     public ResponseEntity<CommonResponse<HubRouteResponseDto>> updateHubRoute(
         @PathVariable UUID route_id,
         @RequestBody
@@ -54,6 +57,7 @@ public class HubRouteController {
 
     @Operation(summary = "허브 루트 삭제")
     @DeleteMapping("/{route_id}")
+    @RequireAuthorization({"MASTER"})
     public ResponseEntity<CommonResponse<HubRouteResponseDto>> deleteHubRoute(
         @PathVariable UUID route_id) {
 
@@ -64,6 +68,7 @@ public class HubRouteController {
 
     @Operation(summary = "허브 루트 검색",description = "허브 루트 Id로 검색합니다.")
     @GetMapping("/{route_id}")
+    @RequireAuthorization({"MASTER", "HUB_MANAGER", "COMPANY_MANAGER","DELIVERY_MANAGER"})
     public ResponseEntity<CommonResponse<HubRouteResponseDto>> getHubRoute(
         @PathVariable UUID route_id) {
 
@@ -74,6 +79,7 @@ public class HubRouteController {
 
     @Operation(summary = "모든 허브루트 조회")
     @GetMapping("/")
+    @RequireAuthorization({"MASTER", "HUB_MANAGER", "COMPANY_MANAGER","DELIVERY_MANAGER"})
     public ResponseEntity<CommonResponse<Page<HubRouteResponseDto>>> getAllHubRoutes(
         @RequestParam int page,
         @RequestParam int size,
@@ -90,6 +96,7 @@ public class HubRouteController {
     @Operation(summary = "허브 루트 검색",description = "키워드를 기준으로 허브 루트를 검색합니다.\n"
         + "키워드는 출발 허브의 이름입니다.")
     @GetMapping("/search")
+    @RequireAuthorization({"MASTER", "HUB_MANAGER", "COMPANY_MANAGER","DELIVERY_MANAGER"})
     public ResponseEntity<CommonResponse<Page<HubRouteResponseDto>>> searchHubRoute(
         @RequestParam int page,
         @RequestParam int size,
@@ -106,6 +113,7 @@ public class HubRouteController {
 
     @Operation(summary = "배달 루트 조회",description = "생성된 허브 루트들을 기준으로 가장 빠른 배달 루트를 생성합니다.")
     @PostMapping("/b2b")
+    @RequireAuthorization({"MASTER", "HUB_MANAGER", "COMPANY_MANAGER","DELIVERY_MANAGER"})
     public Queue<HubRouteResponseDto> findDeliveryRoute(@RequestBody
     HubRouteFindRequestDto requestDto) {
 
@@ -114,6 +122,7 @@ public class HubRouteController {
 
     @Operation(summary = "허브 루트 생성" , description = "현재 데이터베이스에 있는 모든 허브간의 거리정보를 기준으로 허브 루트를 생성합니다.")
     @GetMapping("/generate")
+    @RequireAuthorization({"MASTER"})
     public ResponseEntity<CommonResponse<String>> generateHubRoutes() {
         hubRouteService.generateHubRoutes();
         return CommonResponse.of(CommonResponseCode.CREATED.getCode(),
@@ -122,6 +131,7 @@ public class HubRouteController {
 
     @Operation(summary = "허브간의 거리 및 시간 계산",description = "미리 모든 허브들간의 소요 거리와 시간을 Naver Map direction5 API를 이용해 계산하여 데이터베이스에 저장합니다.")
     @GetMapping("/generate/cal")
+    @RequireAuthorization({"MASTER"})
     public ResponseEntity<CommonResponse<String>> calculateAllHubRoutes() {
         hubRouteService.calculateAllHubRoutes();
         return CommonResponse.of(CommonResponseCode.CREATED.getCode(),
